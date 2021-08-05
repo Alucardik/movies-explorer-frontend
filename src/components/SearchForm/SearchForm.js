a24dd96e1ad5fbeaad6121b91014d4b4c356c192
Alucardik <ichekk@yandex.ru>
2021-08-05 21:44:16 +0300

    feat: make SearchForm adaptive

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,15 +3,15 @@ import './SearchForm.css';
 export default function SearchForm() {
   return(
     <form className="search-form">
-      <fieldset className="search-form__search-container">
+      <div className="search-form__search-container">
         <input
           type="search"
           placeholder="Фильм"
           className="search-form__input"
         />
         <button type="submit" className="search-form__submit-btn" />
-      </fieldset>
-      <fieldset className="search-form__search-options">
+      </div>
+      <div className="search-form__search-options">
         <div className="search-form__search-option">
           Короткометражки
           <label className="search-form__toggle-switch">
@@ -19,7 +19,7 @@ export default function SearchForm() {
             <span className="search-form__toggle-switch-slider" />
           </label>
         </div>
-      </fieldset>
+      </div>
     </form>
   );
 }
